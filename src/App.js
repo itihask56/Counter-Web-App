495a852745328154c991092252a6ada596c330d1
Itihas Verma <itihask56@gmail.com>
2023-10-12 14:45:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+export default function App() {
+  let [val, setVal] = useState(0);
+  // if want to limit the conter: upto certain number you cant increase and decrease
+  if (val<0){
+    alert(" You can't decrease number below 0")
+    setVal(0)
+    
+  }
+  if(val>20){
+    alert("You cant increment more than 20")
+    setVal(20)
+  
+  }
+//........................................//
 
-function App() {
+  const addVal = () => {
+    val = val + 1;
+    setVal(val);
+  };
+  const subtractVal = () => {
+    val = val - 1;
+    setVal(val);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="h-screen grid items-center place-content-center bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+      <h1 className="text-6xl font-bold mb-8">Counter App</h1>
+      <h2 className="text-2xl font-bold mb-8 flex item-center justify-center">
+        Counter Value:{val}
+      </h2>
+      <div className="flex  items-center justify-center space-x-12 text-4xl">
+        <button
+          onClick={addVal}
+          type="button"
+          className="font-medium rounded-full text-sm px-6 py-3  mr-2 mb-2 dark:bg-gray-800 dark:text-white  dark:hover:bg-gray-700  "
         >
-          Learn React
-        </a>
-      </header>
+          Increment
+        </button>
+        <button
+        onClick={subtractVal}
+          type="button"
+          className="font-medium rounded-full text-sm px-6 py-3 mr-2 mb-2 dark:bg-gray-800 dark:text-white  dark:hover:bg-gray-700  "
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
